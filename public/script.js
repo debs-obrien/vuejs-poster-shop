dc469c20e68f0d1d6acced82be6e10fdb6efc955
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-10-27 13:47:46 +0200

    requring a search string

--- public/script.js
+++ public/script.js
@@ -25,17 +25,20 @@ new Vue({
             }
         },
         onSubmit: function(){
-            this.items=[];
+            if(this.newSearch.length){
+                this.items=[];
                 this.loading=true;
-            this.$http
-                .get('/search/'.concat(this.newSearch))
-                .then(function(res){
-                    this.lastSearch = this.newSearch;
-                    this.results =res.data;
-                   this.appendItems();
-                   this.loading=false;
+                this.$http
+                    .get('/search/'.concat(this.newSearch))
+                    .then(function(res){
+                        this.lastSearch = this.newSearch;
+                        this.results =res.data;
+                        this.appendItems();
+                        this.loading=false;
 
-                })
+                    })
+            }
+
         },
         addItem: function(index){
             this.total += 9.99;
