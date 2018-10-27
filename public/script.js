1ff3fe9e2684925122c1c9ebeed050eea39c8e26
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-10-27 12:26:26 +0200

    added default search

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ new Vue({
         total:0,
         items: [],
         cart:[],
-        newSearch: '',
+        newSearch: 'anime',
         lastSearch: '',
         loading: false,
     },
@@ -64,5 +64,9 @@ new Vue({
         currency: function(price){
             return '$'.concat(price.toFixed(2))
         }
+    },
+    mounted: function(){
+        this.onSubmit();
+
     }
 });
